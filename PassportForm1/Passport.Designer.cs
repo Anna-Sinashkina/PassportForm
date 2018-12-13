70e104d7844d5c35d4af1f90f05ae4caf578c351
guest2 <guest2@VUKLEIN>
2018-12-13 15:52:47 +0300

    Add project files.

---
+++ PassportForm1/Passport.Designer.cs
@@ -0,0 +1,280 @@
+﻿namespace PassportForm1
+{
+    partial class Passport
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+       
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.TextBox textBox3;
+            this.label9 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.CountruofBirthPeople = new System.Windows.Forms.TextBox();
+            this.DateofbirthPeople = new System.Windows.Forms.TextBox();
+            this.CountryPeople = new System.Windows.Forms.TextBox();
+            this.PatronymicPeople = new System.Windows.Forms.TextBox();
+            this.NamePerson = new System.Windows.Forms.TextBox();
+            this.SurnamePeople = new System.Windows.Forms.TextBox();
+            this.Date = new System.Windows.Forms.TextBox();
+            this.ByIssue = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.NameOrg = new System.Windows.Forms.TextBox();
+            textBox3 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // textBox3
+            // 
+            textBox3.Location = new System.Drawing.Point(0, 0);
+            textBox3.Name = "textBox3";
+            textBox3.Size = new System.Drawing.Size(100, 20);
+            textBox3.TabIndex = 0;
+            // 
+            // label9
+            // 
+            this.label9.Location = new System.Drawing.Point(134, 576);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(201, 13);
+            this.label9.TabIndex = 38;
+            this.label9.Text = "Город или район рождения владельца";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(134, 509);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(143, 13);
+            this.label8.TabIndex = 37;
+            this.label8.Text = "Дата рождения владельца";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(134, 444);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(200, 13);
+            this.label7.TabIndex = 36;
+            this.label7.Text = "Страна и регион рождения владельца";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(134, 382);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(54, 13);
+            this.label6.TabIndex = 35;
+            this.label6.Text = "Отчество";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(134, 318);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(29, 13);
+            this.label5.TabIndex = 34;
+            this.label5.Text = "Имя";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(131, 258);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(56, 13);
+            this.label4.TabIndex = 33;
+            this.label4.Text = "Фамилия";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(134, 197);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(225, 13);
+            this.label3.TabIndex = 32;
+            this.label3.Text = "Наименование органа,выдавшего паспорт";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(134, 136);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(73, 13);
+            this.label2.TabIndex = 31;
+            this.label2.Text = "Дата выдачи";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(134, 65);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(63, 13);
+            this.label1.TabIndex = 30;
+            this.label1.Text = "Кем выдан";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(151, 680);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(84, 38);
+            this.button1.TabIndex = 29;
+            this.button1.Text = "Сохранить";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // CountruofBirthPeople
+            // 
+            this.CountruofBirthPeople.Location = new System.Drawing.Point(134, 602);
+            this.CountruofBirthPeople.Name = "CountruofBirthPeople";
+            this.CountruofBirthPeople.Size = new System.Drawing.Size(250, 20);
+            this.CountruofBirthPeople.TabIndex = 28;
+            // 
+            // DateofbirthPeople
+            // 
+            this.DateofbirthPeople.Location = new System.Drawing.Point(134, 536);
+            this.DateofbirthPeople.Name = "DateofbirthPeople";
+            this.DateofbirthPeople.Size = new System.Drawing.Size(250, 20);
+            this.DateofbirthPeople.TabIndex = 27;
+            // 
+            // CountryPeople
+            // 
+            this.CountryPeople.Location = new System.Drawing.Point(134, 469);
+            this.CountryPeople.Name = "CountryPeople";
+            this.CountryPeople.Size = new System.Drawing.Size(250, 20);
+            this.CountryPeople.TabIndex = 26;
+            // 
+            // PatronymicPeople
+            // 
+            this.PatronymicPeople.Location = new System.Drawing.Point(134, 407);
+            this.PatronymicPeople.Name = "PatronymicPeople";
+            this.PatronymicPeople.Size = new System.Drawing.Size(250, 20);
+            this.PatronymicPeople.TabIndex = 25;
+            // 
+            // NamePerson
+            // 
+            this.NamePerson.Location = new System.Drawing.Point(134, 346);
+            this.NamePerson.Name = "NamePerson";
+            this.NamePerson.Size = new System.Drawing.Size(250, 20);
+            this.NamePerson.TabIndex = 24;
+            // 
+            // SurnamePeople
+            // 
+            this.SurnamePeople.Location = new System.Drawing.Point(134, 284);
+            this.SurnamePeople.Name = "SurnamePeople";
+            this.SurnamePeople.Size = new System.Drawing.Size(250, 20);
+            this.SurnamePeople.TabIndex = 23;
+            // 
+            // Date
+            // 
+            this.Date.Location = new System.Drawing.Point(134, 162);
+            this.Date.Name = "Date";
+            this.Date.Size = new System.Drawing.Size(250, 20);
+            this.Date.TabIndex = 21;
+            // 
+            // ByIssue
+            // 
+            this.ByIssue.Location = new System.Drawing.Point(134, 99);
+            this.ByIssue.Name = "ByIssue";
+            this.ByIssue.Size = new System.Drawing.Size(250, 20);
+            this.ByIssue.TabIndex = 20;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(355, 680);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(87, 38);
+            this.button2.TabIndex = 39;
+            this.button2.Text = "Отправить";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // NameOrg
+            // 
+            this.NameOrg.Location = new System.Drawing.Point(137, 222);
+            this.NameOrg.Name = "NameOrg";
+            this.NameOrg.Size = new System.Drawing.Size(247, 20);
+            this.NameOrg.TabIndex = 40;
+            // 
+            // Passport
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(657, 793);
+            this.Controls.Add(this.NameOrg);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.CountruofBirthPeople);
+            this.Controls.Add(this.DateofbirthPeople);
+            this.Controls.Add(this.CountryPeople);
+            this.Controls.Add(this.PatronymicPeople);
+            this.Controls.Add(this.NamePerson);
+            this.Controls.Add(this.SurnamePeople);
+            this.Controls.Add(this.Date);
+            this.Controls.Add(this.ByIssue);
+            this.Name = "Passport";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox CountruofBirthPeople;
+        private System.Windows.Forms.TextBox DateofbirthPeople;
+        private System.Windows.Forms.TextBox CountryPeople;
+        private System.Windows.Forms.TextBox PatronymicPeople;
+        private System.Windows.Forms.TextBox NamePerson;
+        private System.Windows.Forms.TextBox SurnamePeople;
+     
+        private System.Windows.Forms.TextBox Date;
+        private System.Windows.Forms.TextBox ByIssue;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox NameOrg;
+    }
+}
+
